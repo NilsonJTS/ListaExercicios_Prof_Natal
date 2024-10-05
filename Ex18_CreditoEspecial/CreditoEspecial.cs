e6fb31b20170b2ae1cfdbc15d409657c09db3ebc
Nilson Jose <nilsonjts@gmail.com>
2024-10-05 15:11:35 -0300

    Exercicio18

---
+++ Ex18_CreditoEspecial/CreditoEspecial.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Ex18_CreditoEspecial;
+
+public class CreditoEspecial
+{      
+    private double saldo;
+
+    public void SetSaldo(double saldo){
+        this.saldo=saldo;
+    }
+
+    public double GetSaldo(){
+        return this.saldo;
+    }
+    
+    public CreditoEspecial(double saldo){
+        this.saldo=saldo;
+        Classifica(this.saldo);
+    }
+
+
+    public void Classifica(double saldo){
+        double saldoMedio=saldo;
+        double porcentagem=0;
+        
+        if(saldoMedio<=200){
+            Console.WriteLine("Seu saldo não lhe dá direito a nenhum crédio.");
+        }else if(saldoMedio>=201 && saldoMedio<=400){
+            porcentagem=1.2;
+            CalculaCredito(porcentagem);
+        }else if(saldoMedio>=401 && saldoMedio<=600){
+            porcentagem=1.3;
+            CalculaCredito(porcentagem);
+        }else{
+            porcentagem=1.4;
+            CalculaCredito(porcentagem);
+        }
+    }
+
+
+    public void CalculaCredito(double porcentagem){
+        double indiceCalculo=porcentagem;
+        double total = saldo*indiceCalculo;
+            Console.WriteLine($"Seu saldo é de R${saldo:n2}.\nSeu crédito é de {(indiceCalculo-1)*100:0}% R${saldo*(indiceCalculo-1):n2}");
+            Console.WriteLine($"Total de R${total:n2}");
+        }
+    
+
+
+
+}
---
+++ Ex18_CreditoEspecial/Program.cs
@@ -0,0 +1,10 @@
+﻿using Ex18_CreditoEspecial;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CreditoEspecial objCredito = new CreditoEspecial(100000);
+
+    }
+}
